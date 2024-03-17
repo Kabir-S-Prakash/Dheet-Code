450ee7765400fdd3aca2d32b7a4c33b38ee00e2a
Kabir S Prakash <kabirsprakash@gmail.com>
2024-03-18 02:15:17 +0530

    Ignoring main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from engine.main import Game
 import scriptblue
-import scriptred
+import script
 
 if __name__ == "__main__":
-    G = Game((40, 40), scriptred, scriptblue)
+    G = Game((40, 40), scriptblue, script)
     G.run_game()
