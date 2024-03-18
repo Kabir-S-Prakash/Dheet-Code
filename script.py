fec3ce00c63ae13bc57d1123a17b5d8cd5dbba77
Kabir S Prakash <kabirsprakash@gmail.com>
2024-03-18 11:31:21 +0530

    Improved script to always return center of any island

--- script.py
+++ script.py
@@ -72,7 +72,7 @@ def spread(pirate):
     x, y = pirate.getPosition()
     
     if( x == 0 , y == 0):
-        return randint(1,4)
+        return randint(2,3)
     
     if(sorted_dict[list(sorted_dict())[3]] == 0 ):
         return randint(1,4)
@@ -92,43 +92,142 @@ def ActPirate(pirate):
     left = pirate.investigate_left()[0]
     right = pirate.investigate_right()[0]
     current = pirate.investigate_current()
+    nw = pirate.investigate_nw()
+    ne = pirate.investigate_ne()
+    se = pirate.investigate_se()
+    sw = pirate.investigate_sw()
     x, y = pirate.getPosition()
     pirate.setSignal("")
     s = pirate.trackPlayers()
     
     if (
-        (up == "island1" and s[0] != "myCaptured")
-        or (up == "island2" and s[1] != "myCaptured")
-        or (up == "island3" and s[2] != "myCaptured")
+        (up == "island1" and nw[0] == "island1" and ne[0] == "island1" and s[0] != "myCaptured")
+        or (up == "island2" and nw[0] == "island2" and ne[0] == "island2" and s[1] != "myCaptured")
+        or (up == "island3" and nw[0] == "island3" and ne[0] == "island3" and s[2] != "myCaptured")
     ):
-        s = up[-1] + str(x) + "," + str(y - 1)
+        s = up[-1] + str(x) + "," + str(y - 2)
         pirate.setTeamSignal(s)
 
     if (
-        (down == "island1" and s[0] != "myCaptured")
-        or (down == "island2" and s[1] != "myCaptured")
-        or (down == "island3" and s[2] != "myCaptured")
+        (up == "island1" and nw[0] == "island1" and ne[0] != "island1" and s[0] != "myCaptured")
+        or (up == "island2" and nw[0] == "island2" and ne[0] != "island2" and s[1] != "myCaptured")
+        or (up == "island3" and nw[0] == "island3" and ne[0] != "island3" and s[2] != "myCaptured")
     ):
-        s = down[-1] + str(x) + "," + str(y + 1)
+        s = up[-1] + str(x-1) + "," + str(y - 2)
         pirate.setTeamSignal(s)
 
     if (
-        (left == "island1" and s[0] != "myCaptured")
-        or (left == "island2" and s[1] != "myCaptured")
-        or (left == "island3" and s[2] != "myCaptured")
+        (up == "island1" and nw[0] != "island1" and ne[0] == "island1" and s[0] != "myCaptured")
+        or (up == "island2" and nw[0] != "island2" and ne[0] == "island2" and s[1] != "myCaptured")
+        or (up == "island3" and nw[0] != "island3" and ne[0] == "island3" and s[2] != "myCaptured")
     ):
-        s = left[-1] + str(x - 1) + "," + str(y)
+        s = up[-1] + str(x+1) + "," + str(y - 2)
         pirate.setTeamSignal(s)
 
     if (
-        (right == "island1" and s[0] != "myCaptured")
-        or (right == "island2" and s[1] != "myCaptured")
-        or (right == "island3" and s[2] != "myCaptured")
+        (down == "island1" and sw[0] == "island1" and se[0] == "island1" and s[0] != "myCaptured")
+        or (down == "island2" and sw[0] == "island2" and se[0] == "island2" and s[1] != "myCaptured")
+        or (down == "island3" and sw[0] == "island3" and se[0] == "island3" and s[2] != "myCaptured")
     ):
-        s = right[-1] + str(x + 1) + "," + str(y)
+        s = down[-1] + str(x) + "," + str(y + 2)
         pirate.setTeamSignal(s)
 
-    if current[1] == 
+    if (
+        (down == "island1" and sw[0] == "island1" and se[0] != "island1" and s[0] != "myCaptured")
+        or (down == "island2" and sw[0] == "island2" and se[0] != "island2" and s[1] != "myCaptured")
+        or (down == "island3" and sw[0] == "island3" and se[0] != "island3" and s[2] != "myCaptured")
+    ):
+        s = down[-1] + str(x-1) + "," + str(y + 2)
+        pirate.setTeamSignal(s)
+
+    if (
+        (down == "island1" and sw[0] != "island1" and se[0] == "island1" and s[0] != "myCaptured")
+        or (down == "island2" and sw[0] != "island2" and se[0] == "island2" and s[1] != "myCaptured")
+        or (down == "island3" and sw[0] != "island3" and se[0] == "island3" and s[2] != "myCaptured")
+    ):
+        s = down[-1] + str(x+1) + "," + str(y + 2)
+        pirate.setTeamSignal(s)
+
+    if (
+        (left == "island1" and nw[0] == "island1" and sw[0] == "island1" and s[0] != "myCaptured")
+        or (left == "island2" and nw[0] == "island2" and sw[0] == "island2" and s[1] != "myCaptured")
+        or (left == "island3" and nw[0] == "island3" and sw[0] == "island3" and s[2] != "myCaptured")
+    ):
+        s = left[-1] + str(x - 2) + "," + str(y)
+        pirate.setTeamSignal(s)
+
+    if (
+        (left == "island1" and nw[0] == "island1" and sw[0] != "island1" and s[0] != "myCaptured")
+        or (left == "island2" and nw[0] == "island2" and sw[0] != "island2" and s[1] != "myCaptured")
+        or (left == "island3" and nw[0] == "island3" and sw[0] != "island3" and s[2] != "myCaptured")
+    ):
+        s = left[-1] + str(x - 2) + "," + str(y-1)
+        pirate.setTeamSignal(s)
+
+    if (
+        (left == "island1" and nw[0] != "island1" and sw[0] == "island1" and s[0] != "myCaptured")
+        or (left == "island2" and nw[0] != "island2" and sw[0] == "island2" and s[1] != "myCaptured")
+        or (left == "island3" and nw[0] != "island3" and sw[0] == "island3" and s[2] != "myCaptured")
+    ):
+        s = left[-1] + str(x - 2) + "," + str(y+1)
+        pirate.setTeamSignal(s)
+
+    if (
+        (right == "island1" and ne[0] == "island1" and se[0] == "island1" and s[0] != "myCaptured")
+        or (right == "island2" and ne[0] == "island2" and se[0] == "island2" and s[1] != "myCaptured")
+        or (right == "island3" and ne[0] == "island3" and se[0] == "island3" and s[2] != "myCaptured")
+    ):
+        s = right[-1] + str(x + 2) + "," + str(y)
+        pirate.setTeamSignal(s)
+
+    if (
+        (right == "island1" and ne[0] == "island1" and se[0] != "island1" and s[0] != "myCaptured")
+        or (right == "island2" and ne[0] == "island2" and se[0] != "island2" and s[1] != "myCaptured")
+        or (right == "island3" and ne[0] == "island3" and se[0] != "island3" and s[2] != "myCaptured")
+    ):
+        s = right[-1] + str(x + 2) + "," + str(y-1)
+        pirate.setTeamSignal(s)
+
+    if (
+        (right == "island1" and ne[0] != "island1" and se[0] == "island1" and s[0] != "myCaptured")
+        or (right == "island2" and ne[0] != "island2" and se[0] == "island2" and s[1] != "myCaptured")
+        or (right == "island3" and ne[0] != "island3" and se[0] == "island3" and s[2] != "myCaptured")
+    ):
+        s = right[-1] + str(x + 2) + "," + str(y+1)
+        pirate.setTeamSignal(s)
+
+    if (
+        (ne[0] == "island1" and up != "island1" and right != "island1" and s[0] != "myCaptured")
+        or (ne[0] == "island2" and up != "island2" and right != "island2" and s[1] != "myCaptured")
+        or (ne[0] == "island3" and up != "island3" and right != "island3" and s[2] != "myCaptured")
+    ):
+        s = ne[0][-1] + str(x+2) + "," + str(y-2)
+        pirate.setTeamSignal(s)
+
+    if (
+        (nw[0] == "island1" and up != "island1" and left != "island1" and s[0] != "myCaptured")
+        or (nw[0] == "island2" and up != "island2" and left != "island2" and s[1] != "myCaptured")
+        or (nw[0] == "island3" and up != "island3" and left != "island3" and s[2] != "myCaptured")
+    ):
+        s = nw[0][-1] + str(x-2) + "," + str(y-2)
+        pirate.setTeamSignal(s)
+
+    if (
+        (sw[0] == "island1" and down != "island1" and left != "island1" and s[0] != "myCaptured")
+        or (sw[0] == "island2" and down != "island2" and left != "island2" and s[1] != "myCaptured")
+        or (sw[0] == "island3" and down != "island3" and left != "island3" and s[2] != "myCaptured")
+    ):
+        s = sw[0][-1] + str(x-2) + "," + str(y+2)
+        pirate.setTeamSignal(s)
+
+    if (
+        (se[0] == "island1" and down != "island1" and right != "island1" and s[0] != "myCaptured")
+        or (se[0] == "island2" and down != "island2" and right != "island2" and s[1] != "myCaptured")
+        or (se[0] == "island3" and down != "island3" and right != "island3" and s[2] != "myCaptured")
+    ):
+        s = se[0][-1] + str(x+2) + "," + str(y+2)
+        pirate.setTeamSignal(s)
+    
     if pirate.getTeamSignal() != "":
         s = pirate.getTeamSignal()
         l = s.split(",")
